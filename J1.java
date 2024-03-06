435528eab6f55c7786075acff4f93e6a8ff76fda
Atousa <139062885+katousa@users.noreply.github.com>
2024-03-06 09:16:02 +0330

    Update J1.java

--- J1.java
+++ J1.java
@@ -15,10 +15,10 @@ import java.util.Scanner;
 public class Program
 {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         int fruit = input.nextInt();
         System.out.print(fruit/2/3);
-       
-       
+       
     }
 }
