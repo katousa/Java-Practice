5a118d747ea7ce07c9f04cf87b4dd5c119573dea
Atousa <139062885+katousa@users.noreply.github.com>
2024-03-06 09:14:16 +0330

    Update

---
+++ j3.java
@@ -0,0 +1,27 @@
+//You go trick or treating with a friend and all but three of the houses that you visit are giving out candy. One house that you visit is giving out toothbrushes and two houses are giving out dollar bills. 
+
+//Task
+//Given the input of the total number of houses that you visited, what is the percentage chance that one random item pulled from your bag is a dollar bill? 
+
+//Input Format 
+//An integer (>=3) representing the total number of houses that you visited. 
+
+//Output Format
+//A percentage value rounded up to the nearest whole number.
+
+import java.util.Scanner;
+
+public class Program
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int houses = input.nextInt();
+         if(200%houses==0){
+          System.out.print(200/houses);
+        }
+        else {System.out.print(200/
+        houses+1);}
+        //your code goes here
+       
+    }
+}
