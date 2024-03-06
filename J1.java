e5c7c1cbe01b9aa833ca2b87f093feea57d8b5f3
Atousa <139062885+katousa@users.noreply.github.com>
2024-03-06 09:05:49 +0330

    Update

---
+++ J1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Program
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int fruit = input.nextInt();
+        System.out.print(fruit/2/3);
+        //your code goes here
+       
+    }
+}
